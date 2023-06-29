f69505a778eb48bbb1733874ab109468b6fcf59d
Neufunk <mail.johnathan@gmail.com>
2023-06-29 13:11:17 +0200

    Initial Commit

---
+++ Lemming/LemmingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemmingApp.swift
+//  Lemming
+//
+//  Created by Johnathan on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct LemmingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
